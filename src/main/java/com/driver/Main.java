f9c1bd0550c103cd49839ded3afe84333ad5a61b
Shilpi <svvar31@gmail.com>
2023-02-08 00:34:35 +0530

    Second Submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj_ect = new RWOnly();
-        obj_ect.name = "Shilpi";
+        /* obj_ect.name = "Shilpi";
         System.out.println(obj_ect.name); // Error : name has private access in com.driver.RWOnly
+        */
 
         System.out.print(obj_ect.getter()); // getter() to access the private variable
         System.out.print(obj_ect.setter("Shilpi")); //  setter() to set the value of the private variable
