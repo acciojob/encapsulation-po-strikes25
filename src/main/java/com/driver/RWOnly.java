ca09ce6f5d8bf1de504f13e4469f4259ecf78d43
Shilpi <svvar31@gmail.com>
2023-02-08 00:30:39 +0530

    First Submit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,4 +1,12 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+    public String getter() {
+        return name;
+    }
+    public String setter(String value) {
+        this.name = value;
+        return name;
+    }
 }
